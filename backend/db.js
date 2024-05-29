89310609f25b29c73dfaa22b9665b9cb13420c06
dakshu04 <dakshgrows@gmail.com>
2024-05-29 19:53:11 +0530

    Starting submodule changes

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,8 @@
 // backend/db.js
 const mongoose = require('mongoose');
 
+mongoose.connect("mongodb://dakshgrows:dakshpurohit04@ac-6mdqwov-shard-00-00.gubcybk.mongodb.net:27017,ac-6mdqwov-shard-00-01.gubcybk.mongodb.net:27017,ac-6mdqwov-shard-00-02.gubcybk.mongodb.net:27017/?replicaSet=atlas-14eba9-shard-0&ssl=true&authSource=admin");
+
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
     username: String,
